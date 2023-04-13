bb50523f0a547a32cd368b6c9d256cfd89c911c8
ddemirovic <dino_demi@web.de>
2023-04-13 15:31:39 +0200

    Finished ChessDictionaryValidator.

--- Chapter 5_dictionaries and structuring data/ChessDictionaryValidator.py
+++ Chapter 5_dictionaries and structuring data/ChessDictionaryValidator.py
@@ -1,26 +1,5 @@
 #A Program that checks if a given chess board is valid or not
 
-# King = König, Queen = Dame, Rook = Turm, Bishop = Läufer, Knight = Springer, Pawn = Bauer
-
-#Chess Board, prefix "w" for "white", prefix "b" for "black" (simple team recognition)
-chessBoard = {'A1': 'wRook' , 'A2': 'wPawn' , 'A3': '' , 'A4': '' , 'A5': '' , 'A6': '' , 'A7': 'bPawn' , 'A8': 'bRook', 
-                'B1': 'wKnight' , 'B2': 'wPawn' , 'B3': '' , 'B4': '' , 'B5': '' , 'B6': '' , 'B7': 'bPawn' , 'B8': 'bKnight', 
-                'C1': 'wBishop' , 'C2': 'wPawn' , 'C3': '' , 'C4': '' , 'C5': '' , 'C6': '' , 'C7': 'bPawn' , 'C8': 'bBishop', 
-                'D1': 'wKing' , 'D2': 'wPawn' , 'D3': '' , 'D4': '' , 'D5': '' , 'D6': '' , 'D7': 'bPawn' , 'D8': 'bQueen', 
-                'E1': 'wQueen' , 'E2': 'wPawn' , 'E3': '' , 'E4': '' , 'E5': '' , 'E6': '' , 'E7': 'bPawn' , 'E8': 'bKing', 
-                'F1': 'wBishop' , 'F2': 'wPawn' , 'F3': '' , 'F4': '' , 'F5': '' , 'F6': '' , 'F7': 'bPawn' , 'F8': 'bBishop', 
-                'G1': 'wKnight' , 'G2': 'wPawn' , 'G3': '' , 'G4': '' , 'G5': '' , 'G6': '' , 'G7': 'bPawn' , 'G8': 'bKnight', 
-                'H1': 'wRook' , 'H2': 'wPawn' , 'H3': '' , 'H4': '' , 'H5': '' , 'H6': '' , 'H7': 'bPawn' , 'H8': 'bRook'}
-
-# chessBoardBackup = {'A1': "wTurm" , 'A2': "wBauer" , 'A3': "" , 'A4': "" , 'A5': "" , 'A6': "" , 'A7': "sBauer" , 'A8': "sTurm", 
-#                 'B1': "wSpringer" , 'B2': "wBauer" , 'B3': "" , 'B4': "" , 'B5': "" , 'B6': "" , 'B7': "sBauer" , 'B8': "sSpringer", 
-#                 'C1': "wLaeufer" , 'C2': "wBauer" , 'C3': "" , 'C4': "" , 'C5': "" , 'C6': "" , 'C7': "sBauer" , 'C8': "sLaeufer", 
-#                 'D1': "wKoenig" , 'D2': "wBauer" , 'D3': "" , 'D4': "" , 'D5': "" , 'D6': "" , 'D7': "sBauer" , 'D8': "sDame", 
-#                 '1': "wDame" , '2': "wBauer" , '3': "" , '4': "" , '5': "" , '6': "" , '7': "sBauer" , '8': "sKoenig", 
-#                 'F1': "wLaeufer" , 'F2': "wBauer" , 'F3': "" , 'F4': "" , 'F5': "" , 'F6': "" , 'F7': "sBauer" , 'F8': "sLaeufer", 
-#                 'G1': "wSpringer" , 'G2': "wBauer" , 'G3': "" , 'G4': "" , 'G5': "" , 'G6': "" , 'G7': "sBauer" , 'G8': "sSpringer", 
-#                 'H1': "wTurm" , 'H2': "wBauer" , 'H3': "" , 'H4': "" , 'H5': "" , 'H6': "" , 'H7': "sBauer" , 'H8': "sTurm"}                
-
 #function that prints the board
 def boardPrint(board):
     print("8", board['A8'], board['B8'], board['C8'], board['D8'], board['E8'], board['F8'], board['G8'], board['H8'])
@@ -32,8 +11,10 @@ def boardPrint(board):
     print("2", board['A2'], board['B2'], board['C2'], board['D2'], board['E2'], board['F2'], board['G2'], board['H2'])
     print("1", board['A1'], board['B1'], board['C1'], board['D1'], board['E1'], board['F1'], board['G1'], board['H1'])
 
-def check_board(board):
-    piece_count = {
+#function that checks if the amount of pieces does not exceed the maximum allowed amount of pieces per team
+def checkPieces(board):
+    #dictionary that contains every possible piece
+    pieceCount = {
         "wRook": 0,
         "wBishop": 0,
         "wKnight": 0,
@@ -47,26 +28,70 @@ def check_board(board):
         "bQueen": 0,
         "bPawn": 0
     }
-    
+
+    piecesValidity = True
+    #pieceCount dictionary values are counted
     for piece in board.values():
         if piece != "":
-            piece_count[piece] += 1
-            
-    for piece_type, count in piece_count.items():
-        if count > 2:
-            if ((piece_type == "wPawn") or (piece_type == "bPawn")) and (count <= 8):
-                # color = piece_type[0]
-                # piece_name = piece_type[1:].lower()
-                # print(color.capitalize(), "team has more than the usual amount of", str(piece_name + "s.A"))
+            pieceCount[piece] += 1
+    
+    #check if one or more pieces exceed the max. amount per team        
+    for pieceType, count in pieceCount.items():
+        if count < 2:   #Max number of king/queen per team = 1, so if the number is lower than 2 (=1), nothing happens, usual amount of number for king/queen
+            continue
+        elif (("wQueen" == pieceType) or ("wKing"== pieceType) or ("bQueen"== pieceType) or ("bKing" == pieceType)) and (count >= 2): #case for more than max w/b queens/kings
+            color = pieceType[0] #team color can be extracted from the first letter of the piece, that's why piece_type[0] = first letter of that string
+            pieceName = pieceType[1:].lower() #the origin string gets separated into two pieces, color and piece_name (this and the line above)
+            print(color.capitalize(), "team has more than the usual amount of", str(pieceName + "s:"), pieceCount[pieceType])
+            piecesValidity = False
+            continue
+        elif count > 2: #Max number of rook/bishop/knight per team = 2, max number of pawn per team = 8, we get here if the number of rook/bishop/knight exceeds 2 and/or if there are the starting amount or more of pawns
+            if (("wPawn" == pieceType) or ("bPawn" == pieceType)) and (count <= 8): #check for white or black pawns, if the number is equal or less than 8 = ok
+                continue
+            elif (("wPawn" == pieceType) or ("bPawn" == pieceType)) and (count > 8): #number of pawns per team is greater than 8 = not ok
+                color = pieceType[0] 
+                pieceName = pieceType[1:].lower() 
+                print(color.capitalize(), "team has more than the usual amount of", str(pieceName + "s:"), pieceCount[pieceType])
+                piecesValidity = False
+                continue
             else:
-                color = piece_type[0]
-                piece_name = piece_type[1:].lower()
-                print(color.capitalize(), "team has more than the usual amount of", str(piece_name + "s.B"))
-                #das kleine f um f-Strings zu erstellen. F-Strings ermöglichen es, Variablen und Ausdrücke direkt in den String einzufügen, anstatt sie als separate Argumente an die Print-Funktion zu übergeben.
-                #print(f"My name is {name} and I am {age} years old.") 
+                color = pieceType[0] 
+                pieceName = pieceType[1:].lower() 
+                print(color.capitalize(), "team has more than the usual amount of", str(pieceName + "s:"), pieceCount[pieceType])
+                piecesValidity = False
+                continue
+            #das kleine f um f-Strings zu erstellen. F-Strings ermöglichen es, Variablen und Ausdrücke direkt in den String einzufügen, anstatt sie als separate Argumente an die Print-Funktion zu übergeben.
+            #print(f"My name is {name} and I am {age} years old.")
+            
+    return piecesValidity 
+
+#function that checks if the spots on the board are valid
+def checkSpots(board):
+    #dictionary that contains every valid chess board position
+    validChessBoardPositions = {
+    'A1': '' , 'A2': '' , 'A3': '' , 'A4': '' , 'A5': '' , 'A6': '' , 'A7': '' , 'A8': '', 
+    'B1': '' , 'B2': '' , 'B3': '' , 'B4': '' , 'B5': '' , 'B6': '' , 'B7': '' , 'B8': '', 
+    'C1': '' , 'C2': '' , 'C3': '' , 'C4': '' , 'C5': '' , 'C6': '' , 'C7': '' , 'C8': '', 
+    'D1': '' , 'D2': '' , 'D3': '' , 'D4': '' , 'D5': '' , 'D6': '' , 'D7': '' , 'D8': '', 
+    'E1': '' , 'E2': '' , 'E3': '' , 'E4': '' , 'E5': '' , 'E6': '' , 'E7': '' , 'E8': '', 
+    'F1': '' , 'F2': '' , 'F3': '' , 'F4': '' , 'F5': '' , 'F6': '' , 'F7': '' , 'F8': '', 
+    'G1': '' , 'G2': '' , 'G3': '' , 'G4': '' , 'G5': '' , 'G6': '' , 'G7': '' , 'G8': '', 
+    'H1': '' , 'H2': '' , 'H3': '' , 'H4': '' , 'H5': '' , 'H6': '' , 'H7': '' , 'H8': ''
+    }
+
+    boardValidity = True
+
+    #check if at least one spot of "board" is invalid
+    for spots in board.keys():
+        if None == validChessBoardPositions.get(spots): #if the current spot from "board" doesn't match any spot from validChessBoardPositions = invalid board
+            print(str(spots) + " is an invalid Spot")
+            boardValidity = False
+    
+    return boardValidity
 
 #functions that checks validity of a board // number of chess pieces (e.g not more than one king per team) & real spots on the board (e.g X3 is not a spot on a usual chess board)
-# def validityCheck(board):
+#my previous attempt, this is still there to remind me where it started
+def validityPieces(board):
 #     wRookChecker = 0
 #     wBishopChecker = 0
 #     wKnightChecker = 0
@@ -113,9 +138,9 @@ def check_board(board):
 #         print("White team has more than the usual amount of bishops.")
 #     if wKnightChecker > 2:
 #         print("White team has more than the usual amount of knights.")
-#     if wKingChecker > 2:
+#     if wKingChecker > 1:
 #         print("White team has more than the usual amount of kings.")
-#     if wQueenChecker > 2:
+#     if wQueenChecker > 1:
 #         print("White team has more than the usual amount of queens.")
 #     if wPawnChecker > 8:
 #         print("White team has more than the usual amount of pawns.")
@@ -125,34 +150,36 @@ def check_board(board):
 #         print("Black team has more than the usual amount of bishops.")
 #     if bKnightChecker > 2:
 #         print("Black team has more than the usual amount of knights.")
-#     if bKingChecker > 2:
+#     if bKingChecker > 1:
 #         print("Black team has more than the usual amount of kings.")
-#     if bQueenChecker > 2:
+#     if bQueenChecker > 1:
 #         print("Black team has more than the usual amount of queens.")
 #     if bPawnChecker > 8:
 #         print("Black team has more than the usual amount of pawns.")
-
-#function that checks if the board is a valid board (if the placement of a chess piece is actually a real spot on the board, e.g A7 = on the board, J9 = not on the board)
-def isValid(board, spot):
-    if board.get(spot) == None:
-        print("Invalid spot")
-    else:
-        print("Valid spot")
-
-#Function that takes an input in order to place a piece
-def whichSpot():
-    pieceSpot = input()
-    piecePlacer(piece, pieceSpot)     #playerTeam as of right now is a global variable, defined way down in the "initialize area"
-
-#function that places the chosen piece
-def piecePlacer(piece, pieceSpot):
     return
 
-# To do: whichSpot als erstes aufrufen, dadrin abfragen ob isValid, 
-# falls ja => whichPiece => piecePlacer (dann für das neue piece eine alte löschen, überlegen wie am besten), vlt. mit get() den Spot besetzen
-# falls nein => Ausgabe machen das der gewählte Spot invalid ist
 
-check_board(chessBoard)
-#print(boardPrint(chessBoard))
-#piece = input()
-#isValid(chessBoard, 'X3')
+    return
+
+def isValidChessBoard(toCheckPieces, toCheckSpots):
+    if (True == toCheckPieces) and (True == toCheckSpots):
+        return True
+    else:
+        return False
+
+#testing area
+# King = König, Queen = Dame, Rook = Turm, Bishop = Läufer, Knight = Springer, Pawn = Bauer
+#Chess Board, prefix "w" for "white", prefix "b" for "black" (simple team recognition)
+chessBoard = {
+    'A1': 'wRook' , 'A2': 'wPawn' , 'A3': '' , 'A4': '' , 'A5': '' , 'A6': '' , 'A7': 'bPawn' , 'A8': 'bRook', 
+    'B1': 'wKnight' , 'B2': 'wPawn' , 'B3': '' , 'B4': '' , 'B5': '' , 'B6': '' , 'B7': 'bPawn' , 'B8': 'bKnight', 
+    'C1': 'wBishop' , 'C2': 'wPawn' , 'C3': '' , 'C4': '' , 'C5': '' , 'C6': '' , 'C7': 'bPawn' , 'C8': 'bBishop', 
+    'D1': 'wKing' , 'D2': 'wPawn' , 'D3': '' , 'D4': '' , 'D5': '' , 'D6': '' , 'D7': 'bPawn' , 'D8': 'bQueen', 
+    'E1': 'wQueen' , 'E2': 'wPawn' , 'E3': '' , 'E4': '' , 'E5': '' , 'E6': '' , 'E7': 'bPawn' , 'E8': 'bKing', 
+    'F1': 'wBishop' , 'F2': 'wPawn' , 'F3': '' , 'F4': '' , 'F5': '' , 'F6': '' , 'F7': 'bPawn' , 'F8': 'bBishop', 
+    'G1': 'wKnight' , 'G2': 'wPawn' , 'G3': '' , 'G4': '' , 'G5': '' , 'G6': '' , 'G7': 'bPawn' , 'G8': 'bKnight', 
+    'H1': 'wRook' , 'H2': 'wPawn' , 'H3': '' , 'H4': '' , 'H5': '' , 'H6': '' , 'H7': 'bPawn' , 'H8': 'bRook'
+}    
+checker1 = checkPieces(chessBoard)
+checker2= checkSpots(chessBoard)
+print(isValidChessBoard(checker1, checker2))
