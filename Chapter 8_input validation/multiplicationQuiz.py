b232158106885abf0c92d7b8b14bba273a95971b
DDemirovic <111425664+DDemirovic@users.noreply.github.com>
2023-08-21 10:03:47 +0200

    finished multiplicationQuiz.py

--- Chapter 8_input validation/multiplicationQuiz.py
+++ Chapter 8_input validation/multiplicationQuiz.py
@@ -1,42 +1,42 @@
 # #! python 3
 # #multiplicationQuiz.py - a programm that quizzes the user a certain number of times, used to get a feel on how much work pyinputplus is doing (for input validation)
 
-# // TODO timeout. 
-
 import random
 import time
 
 numberOfQuestions = 10
 correctAnswers = 0
-incorrectAttempts = 0
 
 for questionNumber in range(1, numberOfQuestions+1):
+    #generating random numbers and resetting incorrectAttempts for each iteration in the loop 
     numberOne = random.randint(0,10)
     numberTwo = random.randint(0,10)
     multiplicand = numberOne * numberTwo
-    time.sleep(1)
+    incorrectAttempts = 0
+    time.sleep(1)   #adding several sleep commands, so the user can rest in between the questions
+    #initializing timer_start and timer_end
     timer_start = int(time.perf_counter())
-    userInput = input(f"{questionNumber}: What's {numberOne} x {numberTwo}? ")
     timer_end = int(time.perf_counter())
+    userInput = input(f"{questionNumber}: What's {numberOne} x {numberTwo}?\n")
     #user gets 3 chances per question to input an integer
     while incorrectAttempts < 2 and userInput.isdigit() == False and timer_end - timer_start < 8:
-        timer_end = int(time.perf_counter())
         incorrectAttempts += 1
         print(f"{userInput} is not a number!")
-        userInput = input(f"{questionNumber}: Try again! Attempt: {incorrectAttempts+1}\nWhat's {numberOne} x {numberTwo}? ")
+        userInput = input(f"{questionNumber}: Try again!\nAttempt: {incorrectAttempts+1}\nWhat's {numberOne} x {numberTwo}?\n")
+        timer_end = int(time.perf_counter())
     #user gets 3 chances per question to answer correctly (math problem, not an input problem)
     while incorrectAttempts < 2 and int(userInput) != multiplicand:
-        timer_end = int(time.perf_counter())
         incorrectAttempts += 1
         print("Incorrect! :(")
-        userInput = input(f"{questionNumber}: Try again! Attempt: {incorrectAttempts+1}\nWhat's {numberOne} x {numberTwo}? ")
+        userInput = input(f"{questionNumber}: Try again!\nAttempt: {incorrectAttempts+1}\nWhat's {numberOne} x {numberTwo}?\n")
+        timer_end = int(time.perf_counter())
     #printing an incorrect message if user exceeded 3 attempts
     if incorrectAttempts == 2:
         incorrectAttempts = 0
         print("Still incorrect. Exceeded your 3 attempts. :(")
         time.sleep(1)
         continue
-    timer_end = int(time.perf_counter())
+    timer_end = int(time.perf_counter())    #getting new timer_end 
     #printing timeout message if timeout is reached
     if timer_end - timer_start > 8:
         print("It took you longer than 8 seconds.")
@@ -48,7 +48,7 @@ for questionNumber in range(1, numberOfQuestions+1):
         print("Correct!")
         correctAnswers += 1
         time.sleep(1)
-    incorrectAttempts = 0
-print(f"You answered {correctAnswers}/{numberOfQuestions} correctly.")
+    #incorrectAttempts = 0
+print(f"You answered {correctAnswers}/{numberOfQuestions} correctly.")  #congratulating the user 
 
 #if __name__ == '__main__':
