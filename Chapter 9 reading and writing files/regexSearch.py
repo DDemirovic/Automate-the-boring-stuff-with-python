497193aecc06b6ba76efe5eb3277e8c9a36c5150
ddemirovic <dino_demi@web.de>
2023-08-29 20:55:14 +0200

    tweaked regexSearch.py - need to finish working on the text from those files (regex search etc.)

--- Chapter 9 reading and writing files/regexSearch.py
+++ Chapter 9 reading and writing files/regexSearch.py
@@ -6,13 +6,20 @@ import pathlib
 import os
 
 #hardcoded path of the files which we wanna scan - obviously change the path if you want to scan other .txt files
-myPath = pathlib.Path("G:\\Automate the boring stuff with Python\\Chapter 9_reading and writing files\\bunchOfTextFiles")
+myPath = pathlib.Path("G:\\Automate the boring stuff with Python\\Chapter 9 reading and writing files\\bunchOfTextFiles")
+listOfTextFiles = []    #empty list - storing all the names of the .txt files found in that path
 
-#loop that iterates over every fileName in the specified path
-for fileName in os.listdir(myPath):
-    print(fileName)
+#loop that searches for text files in the specified path and adds that path to the empty list
+i = 0
+for textFile in myPath.glob('*.txt'):
+    stringOfTextFilePath = ('\\'.join([str(myPath), str(textFile.name)]))
+    p = pathlib.Path(stringOfTextFilePath)
+    temp = p.read_text()
+    print(temp)
+    # os.startfile(listOfTextFiles[i])
+    # i += 1
 
-print(list(myPath.glob('*.txt')))
+
 
 #das unten stehende benötige ich um .txt files zu filtern
 #   dann mit myPath.name nur den Namen der .txt zu kriegen
